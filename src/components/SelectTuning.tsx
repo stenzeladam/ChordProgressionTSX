a2fc6eae72b9cbd67731a3032aac09ee265c84e6
Dana Alnusair <dana.nusair88@gmail.com>
2024-07-01 00:26:24 +0300

    Fixes bug in single string tuning selector, modifies the indentation of file

--- src/components/SelectTuning.tsx
+++ src/components/SelectTuning.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Box from '@mui/material/Box';
 
-const Tuning = [
+const tuningOptions = [
   { label: 'E Standard', tuning: "E Standard" },
   { label: 'Eb/D# Standard', tuning: "D# Standard" },
   { label: 'D Standard', tuning: "D Standard" },
@@ -35,6 +35,7 @@ const standardTunings: { [key: string]: string[] } = {
 };
 
 export default function ComboBox() {
+  // this seems to not be used anywhere, is it needed for later?
   const [selectedTuning, setSelectedTuning] = useState<string | null>(null);
   const [stringTunings, setStringTunings] = useState<string[]>(Array(6).fill(""));
 
@@ -48,69 +49,85 @@ export default function ComboBox() {
       setStringTunings(Array(6).fill(""));
     }
 
-    console.log("The selected tuning is: ", tuning);
+    console.log("The selected tuning is: ", tuning, value?.label);
   };
+
+  // Function to allow user to individually modify the string's tuning
+  // Takes the user's desired value and places it in the tuning array based on the index
+  const handleSingleStringTuningChange = (value: string | null, index: number) => {
+    setStringTunings((prevStringTuning: string[]) => {
+      prevStringTuning[index] = value ?? ''
+
+      return [...prevStringTuning] // Creates a new copy of the array
+    })
+  }
 
   return (
     <div>
       <Autocomplete
         disablePortal
         id="combo-box-tuning"
-        options={Tuning}
+        options={tuningOptions}
         sx={{ width: 300, marginBottom: 2 }}
         renderInput={(params) => <TextField {...params} label="Select a tuning" />}
         onChange={handleTuningChange}
       />
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
-              <Autocomplete
-                  key={"6thString"}
-                  disablePortal
-                  value={stringTunings[0] || null}
-                  options={["E", "Eb/D#", "D", "Db/C#", "C", "B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#", "F"]}
-                  sx={{ width: 200 }}
-                  renderInput={(params) => <TextField {...params} label={`6th String (Lowest)`} size="small"/>}
-              />
-              <Autocomplete
-                  key={"5thString"}
-                  disablePortal
-                  value={stringTunings[1] || null}
-                  options={["A", "Ab/G#", "G", "Gb/F#", "F", "E", "Eb/D#", "D", "Db/C#", "C", "B", "Bb/A#"]}
-                  sx={{ width: 200}}
-                  renderInput={(params) => <TextField {...params} label={`5th String`} size="small"/>}
-              />
-              <Autocomplete
-                  key={"4thString"}
-                  disablePortal
-                  value={stringTunings[2] || null}
-                  options={["D", "Db/C#", "C", "B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#", "F", "E", "Eb/D#"]}
-                  sx={{ width: 200}}
-                  renderInput={(params) => <TextField {...params} label={`4th String`} size="small"/>}
-              />
-              <Autocomplete
-                  key={"3rdString"}
-                  disablePortal
-                  value={stringTunings[3] || null}
-                  options={["G", "Gb/F#", "F", "E", "Eb/D#", "D", "Db/C#", "C", "B", "Bb/A#", "A", "Ab/G#"]}
-                  sx={{ width: 200}}
-                  renderInput={(params) => <TextField {...params} label={`3rd String`} size="small"/>}
-              />
-              <Autocomplete
-                  key={"2ndString"}
-                  disablePortal
-                  value={stringTunings[4] || null}
-                  options={["B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#", "F", "E", "Eb/D#", "D", "Db/C#", "C"]}
-                  sx={{ width: 200}}
-                  renderInput={(params) => <TextField {...params} label={`2nd String`} size="small"/>}
-              />
-              <Autocomplete
-                  key={"1stString"}
-                  disablePortal
-                  value={stringTunings[5] || null}
-                  options={["E", "Eb/D#", "D", "Db/C#", "C", "B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#", "F"]}
-                  sx={{ width: 200}}
-                  renderInput={(params) => <TextField {...params} label={`1st String (Highest)`} size="small"/>}
-              />
-            </Box>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
+        <Autocomplete
+            key={"6thString"}
+            disablePortal
+            value={stringTunings[0] || null}
+            options={["E", "Eb/D#", "D", "Db/C#", "C", "B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#", "F"]}
+            sx={{ width: 200 }}
+            renderInput={(params) => <TextField {...params} label={`6th String (Lowest)`} size="small"/>}
+            onChange={(_, value) => handleSingleStringTuningChange(value, 0)}
+        />
+        <Autocomplete
+            key={"5thString"}
+            disablePortal
+            value={stringTunings[1] || null}
+            options={["A", "Ab/G#", "G", "Gb/F#", "F", "E", "Eb/D#", "D", "Db/C#", "C", "B", "Bb/A#"]}
+            sx={{ width: 200}}
+            renderInput={(params) => <TextField {...params} label={`5th String`} size="small"/>}
+            onChange={(_, value) => handleSingleStringTuningChange(value, 1)}
+        />
+        <Autocomplete
+            key={"4thString"}
+            disablePortal
+            value={stringTunings[2] || null}
+            options={["D", "Db/C#", "C", "B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#", "F", "E", "Eb/D#"]}
+            sx={{ width: 200}}
+            renderInput={(params) => <TextField {...params} label={`4th String`} size="small"/>}
+            onChange={(_, value) => handleSingleStringTuningChange(value, 2)}
+        />
+        <Autocomplete
+            key={"3rdString"}
+            disablePortal
+            value={stringTunings[3] || null}
+            options={["G", "Gb/F#", "F", "E", "Eb/D#", "D", "Db/C#", "C", "B", "Bb/A#", "A", "Ab/G#"]}
+            sx={{ width: 200}}
+            renderInput={(params) => <TextField {...params} label={`3rd String`} size="small"/>}
+            onChange={(_, value) => handleSingleStringTuningChange(value, 3)}
+        />
+        <Autocomplete
+            key={"2ndString"}
+            disablePortal
+            value={stringTunings[4] || null}
+            options={["B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#", "F", "E", "Eb/D#", "D", "Db/C#", "C"]}
+            sx={{ width: 200}}
+            renderInput={(params) => <TextField {...params} label={`2nd String`} size="small"/>}
+            onChange={(_, value) => handleSingleStringTuningChange(value, 4)}
+        />
+        <Autocomplete
+            key={"1stString"}
+            disablePortal
+            value={stringTunings[5] || null}
+            options={["E", "Eb/D#", "D", "Db/C#", "C", "B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#", "F"]}
+            sx={{ width: 200}}
+            renderInput={(params) => <TextField {...params} label={`1st String (Highest)`} size="small"/>}
+            onChange={(_, value) => handleSingleStringTuningChange(value, 5)}
+        />
+      </Box>
     </div>
   );
 }
